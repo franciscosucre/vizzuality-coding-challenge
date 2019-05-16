e5ad4701092873e5943165bf9ead6bf0b0327f52
30059200 <francisco-javier.sucre@ext.leroymerlin.es>
2019-05-16 11:12:48 +0200

    fixes in route return

--- reader/middleware.js
+++ reader/middleware.js
@@ -1,48 +1,48 @@
-const { MongoDbQueryParams } = require('@sugo/mongodb-queryparams')
-
-const logger = require('./logger')
+const { MongoDbQueryParams } = require('@sugo/mongodb-queryparams');
+const util = require('util');
+const logger = require('./logger');
 
 exports.handleError = async (req, res, next) => {
   try {
-    await next()
+    await next();
   } catch (err) {
     const defaultValues = {
       code: 'N/A',
       message: 'Unexpected Error',
       name: err.name ? err.name : err.constructor.name ? err.constructor.name : 'Error',
-      status: 500
-    }
+      status: 500,
+    };
     const json = Object.getOwnPropertyNames(err).reduce((obj, key) => {
-      obj[key] = err[key]
-      return obj
-    }, defaultValues)
-    res.status(json.status).json(json)
-    const { id, statusCode, statusMessage, body, method, url } = res
+      obj[key] = err[key];
+      return obj;
+    }, defaultValues);
+    res.status(json.status).json(json);
+    const { id, statusCode, statusMessage, body, method, url } = res;
     const log = `Response ID: ( ${id} ) ${method}: ${url} ${statusCode} ${statusMessage} ---> body: ${JSON.stringify(
-      body
-    )}`
-    logger.error(log)
+      body,
+    )}`;
+    logger.error(log);
   }
-}
+};
 
 exports.logRequest = async (req, res, next) => {
-  let log = util.format('Request ID: ( %s ) %s: %s', req.id, req.method, req.url)
-  log += util.format(' --> query %j', req.query)
-  log += util.format(' --> body %j', req.body)
-  logger.info(log)
-  return next ? await next() : null
-}
+  let log = util.format('Request ID: ( %s ) %s: %s', req.id, req.method, req.url);
+  log += util.format(' --> query %j', req.query);
+  log += util.format(' --> body %j', req.body);
+  logger.info(log);
+  return next ? await next() : null;
+};
 
 exports.logResponse = async (req, res, next) => {
-  next ? await next() : null
-  const { id, statusCode, statusMessage, body, method, url } = res
+  next ? await next() : null;
+  const { id, statusCode, statusMessage, body, method, url } = res;
   const log = `Response ID: ( ${id} ) ${method}: ${url} ${statusCode} ${statusMessage} ---> body: ${JSON.stringify(
-    body
-  )}`
-  logger.info(log)
-}
+    body,
+  )}`;
+  logger.info(log);
+};
 
 exports.parseQueryParams = async (req, res, next) => {
-  req.query = MongoDbQueryParams.parseQueryParams(req.query)
-  return next ? await next() : null
-}
+  req.query = MongoDbQueryParams.parseQueryParams(req.query);
+  return next ? await next() : null;
+};
--- reader/router.js
+++ reader/router.js
@@ -1,10 +1,11 @@
-const { Router } = require('@sugo/router')
-const db = require('./mongodb')
+const { Router } = require('@sugo/router');
+const db = require('./mongodb');
 
 const router = new Router().get('/', async (req, res) => {
-  const col = await db.getEntriesCollection()
-  const { limit, skip, filter, select: projection, sort } = req.query
-  return col.find(filter, { limit, sort, projection, skip }).toArray()
-})
+  const col = await db.getEntriesCollection();
+  const { limit, skip, filter, select: projection, sort } = req.query;
+  const docs = await col.find(filter, { limit, sort, projection, skip }).toArray();
+  return res.status(200).json(docs);
+});
 
-module.exports = router
+module.exports = router;

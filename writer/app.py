eeead8d3133016663bd3c99e84dc368ed551a3a9
30059200 <francisco-javier.sucre@ext.leroymerlin.es>
2019-05-16 10:58:58 +0200

    writer implemented

--- writer/app.py
+++ writer/app.py
@@ -1,14 +1,26 @@
-import os
+# Standard libs imports
 import io
-from flask import Flask, flash, request, redirect, url_for, make_response
+import os
 import csv
+
+# Third party libs imports
+from flask import Flask, request, jsonify
+from pymongo import InsertOne
+from flask_pymongo import PyMongo
 from werkzeug.utils import secure_filename
 
+app = Flask(__name__)
+
+# File Upload
 UPLOAD_FOLDER = './tmp'
 ALLOWED_EXTENSIONS = set(['csv'])
-
-app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# MongoDB
+default_uri = 'mongodb://localhost:27017/vizzuality-code-challenge'
+mongodb_uri = os.environ.get('MONGODB_URI', default_uri)
+app.config["MONGO_URI"] = mongodb_uri
+mongo = PyMongo(app)
 
 
 def allowed_file(filename):
@@ -18,27 +30,25 @@ def allowed_file(filename):
 
 @app.route('/', methods=['POST'])
 def upload_file():
-    # check if the post request has the file part
     if 'file' not in request.files:
         raise Exception('No file part')
     file = request.files['file']
-    # if user does not select file, browser also
-    # submit an empty part without filename
     if file.filename == '':
         raise Exception('No selected file')
     allowed_file(file.filename)
     filename = secure_filename(file.filename)
 
     stream = io.StringIO(file.stream.read().decode("UTF8"), newline=None)
-    csv_input = csv.reader(stream)
-    #print("file contents: ", file_contents)
-    # print(type(file_contents))
-    print(csv_input)
+    csv_input = csv.DictReader(stream)
+
+    operations = []
+
     for row in csv_input:
-        print(row)
+        operations.append(InsertOne(dict(row)))
 
-    stream.seek(0)
-    result = stream.read()
+    result = mongo.db.entries.bulk_write(operations)
+    print(result.inserted_count)
 
-    response = make_response(result)
-    return {'message': 'ok'}
+    return jsonify({
+        'inserted_count': result.inserted_count
+    })

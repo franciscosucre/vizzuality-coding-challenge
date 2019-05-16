bf6d168f882ccea2a59275b5cad0d33c09f787bc
30059200 <francisco-javier.sucre@ext.leroymerlin.es>
2019-05-16 13:14:59 +0200

    fixes in queryparam parsing

--- reader/router.js
+++ reader/router.js
@@ -1,11 +1,14 @@
-const { Router } = require('@sugo/router');
-const db = require('./mongodb');
+const { Router } = require('@sugo/router')
+const db = require('./mongodb')
 
 const router = new Router().get('/', async (req, res) => {
-  const col = await db.getEntriesCollection();
-  const { limit, skip, filter, select: projection, sort } = req.query;
-  const docs = await col.find(filter, { limit, sort, projection, skip }).toArray();
-  return res.status(200).json(docs);
-});
+  const col = await db.getEntriesCollection()
+  const { limit, skip, filter, select: projection, sort } = req.query
+  const [docs, count] = await Promise.all([
+    col.find(filter, { limit, sort, projection, skip }).toArray(),
+    col.countDocuments(filter)
+  ])
+  return res.status(200).json({ docs, count })
+})
 
-module.exports = router;
+module.exports = router
